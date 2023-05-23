317b9fa37bb448e1993760d1be6582838e77590e
ChristerNilsson <janchrister.nilsson@gmail.com>
2023-05-23 23:23:28 +0200

    undo

--- BastardChess.py
+++ BastardChess.py
@@ -1,8 +1,6 @@
 # TODO
 
-# Visa initiala drag
-# Se över promovering
-# Dubbla bräden?
+# Undo
 
 import PySimpleGUI as sg
 import chess
@@ -11,12 +9,22 @@ import chess.engine
 import pyperclip
 import subprocess
 
-URVAL = "234" # Första index är ett. Bästa tre dragen visas förutom det bästa.
-SECONDS = 0.1 # thinking time
-ORDER = 'Alfa'
-PROMO = 'Dam'
+ENGINE = "C:/github/2023-018-Python-Chess_Evaluate/stockfish15/stockfish-windows-2022-x86-64-modern.exe"
 BROWSER = "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
 HELP = "https://github.com/ChristerNilsson/2023-028-BastardChess#bastardchess"
+HEADER = ' Nr|  Vit  | Svart '.split('|')
+
+CLUES = '0 123 1234 12345 234 2345 23456'.split(' ')
+CLUE = "234" # Första index är ett. Bästa tre dragen visas förutom det bästa.
+
+TIMES = [10,20,50,100,200,500,1000,2000] # milliseconds
+TIME = 200 # thinking time
+
+ORDERS = 'Alfabetisk Styrka'.split(' ')
+ORDER = 'Alfabetisk'
+
+PROMOS = 'Dam Torn Löpare Springare'.split(' ')
+PROMO = 'Dam'
 
 finalized = False
 stack = [] # san moves for gui
@@ -26,35 +34,20 @@ BLANK = 0
 window = None
 initial_board = 'r n b q k b n r\np p p p p p p p\n. . . . . . . .\n. . . . . . . .\n. . . . . . . .\n. . . . . . . .\nP P P P P P P P\nR N B Q K B N R'
 
-blank   = './blank.png'
-
-bishopB = './images/bb.png'
-bishopW = './images/wb.png'
-pawnB   = './images/bp.png'
-pawnW   = './images/wp.png'
-knightB = './images/bn.png'
-knightW = './images/wn.png'
-rookB   = './images/br.png'
-rookW   = './images/wr.png'
-queenB  = './images/bq.png'
-queenW  = './images/wq.png'
-kingB   = './images/bk.png'
-kingW   = './images/wk.png'
-
 images = {
-	'.':blank,
-	'p':pawnB,
-	'n':knightB,
-	'b':bishopB,
-	'r':rookB,
-	'q':queenB,
-	'k':kingB,
-	'P':pawnW,
-	'N':knightW,
-	'B':bishopW,
-	'R':rookW,
-	'Q':queenW,
-	'K':kingW}
+	'.':'blank',
+	'p':'bp',
+	'n':'bn',
+	'b':'bb',
+	'r':'br',
+	'q':'bq',
+	'k':'bk',
+	'P':'wp',
+	'N':'wn',
+	'B':'wb',
+	'R':'wr',
+	'Q':'wq',
+	'K':'wk'}
 
 def render_square(image, key, location):
 	color = '#B58863' if (location[0] + location[1]) % 2 else '#F0D9B5'
@@ -65,71 +58,80 @@ def redraw_board(window, board):
 	for i in range(8):
 		for j in range(8):
 			color = '#B58863' if (i + j) % 2 else '#F0D9B5'
-			piece_image = images[b[i][j]]
+			piece_image = './images/' + images[b[i][j]] + '.png'
 			elem = window[(i, j)]
 			elem.Update(button_color=('white', color), image_filename=piece_image, image_size=(75, 75), image_subsample=2, )
 
-def clues(engine,board):
-	global SECONDS, URVAL, ORDER, PROMO
-	if not finalized: return []
-	SECONDS = window['_seconds_'].get()
-	URVAL = window['_urval_'].get()
-	maximum = int(URVAL[-1])
-	ORDER = window['_order_'].get()
-	PROMO = window['_promo_'].get()
+def hor(a,b): return [sg.Column([[a,b]])]
 
-	info = engine.analyse(board, chess.engine.Limit(time=SECONDS), multipv=maximum)
-	n = len(info)
-	best_moves = []
-	for ch in URVAL:
-		i = int(ch)-1
-		if len(best_moves) < n:
-			best_moves.append(board.san(info[i]['pv'][0]))
-	if ORDER == 'Alfa':
-		best_moves.sort()
-	return best_moves
+def makeHistory():
+	pgn = []
+	pgnpc = []
+	for i in range(0, len(stack), 2):
+		t = [str(i // 2 + 1), stack[i]]
+		if i + 1 < len(stack): t.append(stack[i + 1])
+		pgn.append(t)
+		pgnpc.append(' '.join(t))
+	pyperclip.copy("\n".join(pgnpc))
+	return pgn
 
 def PlayGame():
-	global window, SECONDS, finalized, stack
+	global window, ORDER, TIME, CLUE, PROMO, finalized, stack
+
+	def clues(engine, board):
+		maximum = int(CLUE[-1])
+		if maximum == 0: return []
+		info = engine.analyse(board, chess.engine.Limit(time=TIME / 1000), multipv=maximum)
+		n = len(info)
+		best_moves = []
+		for ch in CLUE:
+			i = int(ch) - 1
+			if len(best_moves) < n:
+				best_moves.append(board.san(info[i]['pv'][0]))
+		if ORDER == 'Alfabetisk': best_moves.sort()
+		return best_moves
 
 	b = [row.split(' ') for row in str(initial_board).split("\n")]
 	board_layout = []
 	for i in range(8):
 		row = []
 		for j in range(8):
-			piece_image = images[b[i][j]]
+			piece_image = './images/' + images[b[i][j]] + '.png'
 			row.append(render_square(piece_image, key=(i, j), location=(i, j)))
 		board_layout.append(row)
 
-	filename = "C:/github/2023-018-Python-Chess_Evaluate/stockfish15/stockfish-windows-2022-x86-64-modern.exe"
-	engine = chess.engine.SimpleEngine.popen_uci(filename)
+	engine = chess.engine.SimpleEngine.popen_uci(ENGINE)
 	board = chess.Board()
 
 	a = sg.Text('Sortering')
-	b = sg.Combo(['Alfa','Styrka'], size=(10, 2), readonly=True, default_value=ORDER, key='_order_')
-	c = sg.Column([[a,b]])
-	d = sg.Text('Sekunder')
-	e = sg.Combo([0.001,0.01,0.1,1], size=(10, 4), readonly=True, default_value=SECONDS, key='_seconds_')
-	f = sg.Column([[d,e]])
-	g = sg.Text('Ledtrådar')
-	h = sg.Combo('123 1234 12345 234 2345 23456'.split(' '), size=(10, 6), readonly=True, default_value=URVAL, key='_urval_')
-	k = sg.Column([[g,h]])
-	p1 = sg.Text('Promovering')
-	p2 = sg.Combo('Dam Torn Löpare Springare'.split(' '), size=(10, 4), readonly=True, default_value=PROMO, key='_promo_')
-	p3 = sg.Column([[p1,p2]])
-	board_controls = [
-		[c],
-		[f],
-		[p3],
-		[k],
-		[sg.Text(' '.join(clues(engine,board)), size=(22, 2),  key='_clues_'),],
-		[sg.Text('Historik')],
-		[sg.Table(values = [], headings=['Nr',' White ',' Black '],size=(10,10),key='_historik_',justification = "center", hide_vertical_scroll=True)],
+	b = sg.Combo(ORDERS, size=(10, 10), readonly=True, default_value=ORDER, key='_order_')
 
-		[sg.Button('Hjälp')],
-		[sg.Button('Analys')],
-		[sg.Button('Nytt parti')],
-		[sg.Button('Avsluta')],
+	d = sg.Text('Millisekunder')
+	e = sg.Combo(TIMES, size=(8, 10), readonly=True, default_value=TIME, key='_TIME_')
+
+	g = sg.Text('Ledtrådar')
+	h = sg.Combo(CLUES, size=(10, 10), readonly=True, default_value=CLUE, key='_clue_')
+
+	p1 = sg.Text('Promovering')
+	p2 = sg.Combo(PROMOS, size=(10, 10), readonly=True, default_value=PROMO, key='_promo_')
+
+	q1 = sg.Button('Hjälp')
+	q2 = sg.Button('Ångra')
+
+	r1 = sg.Button('Analys')
+	r2 = sg.Button('Nytt parti')
+
+	board_controls = [
+		hor(a,b),
+		hor(d,e),
+		hor(p1,p2),
+		hor(g,h),
+		[sg.Text(' '.join(clues(engine,board)), size=(22, 2),  key='_clues_'),],
+		[sg.Text('Tio senaste dragen')],
+		[sg.Table(values = [], headings=HEADER,size=(10,10),key='_historik_',justification = "center", num_rows=10, hide_vertical_scroll=True)],
+		hor(q1,q2),
+		hor(r1,r2),
+		[sg.Button('Avsluta')]
 	]
 
 	layout = [[sg.Column(board_layout), sg.Column(board_controls)]]
@@ -137,19 +139,22 @@ def PlayGame():
 	window = sg.Window('Bastardschack',
 	default_button_element_size=(10, 1),
 	auto_size_buttons=False,
-	font='Arial 12',
+	font='Arial 16',
 	icon='kingb.ico').Layout(layout)
 
 	while not board.is_game_over():
-
-		SECONDS = window['_seconds_']
-
 		if finalized:
 			window['_clues_'].Update(' '.join(clues(engine, board)))
 
 		move_state = 0
 		while True:
 			button, value = window.Read()
+
+			ORDER = value['_order_']
+			TIME = value['_TIME_']
+			PROMO = value['_promo_']
+			CLUE = value['_clue_']
+
 			if button in (None, 'Exit'): exit()
 			if button == 'Avsluta':
 				engine.quit()
@@ -158,7 +163,7 @@ def PlayGame():
 			if button == 'Nytt parti':
 				board = chess.Board()
 				stack = []
-				window['_clues_'].Update('')
+				window['_clues_'].Update(' '.join(clues(engine,board)))
 				window['_historik_'].Update('')
 				redraw_board(window,board)
 			if button == 'Hjälp':
@@ -167,7 +172,16 @@ def PlayGame():
 			if button == 'Analys':
 				subprocess.Popen([BROWSER ,"https:\\lichess.org\paste"])
 				break
-			if type(button) is tuple:
+			if button == 'Ångra':
+				if len(stack) == 0: break
+				board.pop()
+				stack.pop()
+				redraw_board(window,board)
+				pgn = makeHistory()
+				window['_historik_'].Update(pgn[-10:])
+				# print(len(board.move_stack),len(stack))
+				break
+			if type(button) is tuple: # en av 64 rutor
 				if move_state == 0:
 					move_from = button
 					row, col = move_from
@@ -193,17 +207,11 @@ def PlayGame():
 
 					if (piece == 'P' and 8 - move_to[0] == 8) or (piece == 'p' and 8 - move_to[0] == 1):
 						picked_move += {'Dam':'q','Torn':'r','Löpare':'b','Springare':'n'}[PROMO]
+						print(picked_move)
 
 					if picked_move in [str(move) for move in board.legal_moves]:
 						stack.append(board.san(chess.Move.from_uci(picked_move)))
-						pgn = []
-						pgnpc = []
-						for i in range(0,len(stack),2):
-							t = [str(i // 2 + 1), stack[i]]
-							if i+1 < len(stack): t.append(stack[i + 1])
-							pgn.append(t)
-							pgnpc.append(' '.join(t))
-						pyperclip.copy("\n".join(pgnpc))
+						pgn = makeHistory()
 						board.push(chess.Move.from_uci(picked_move))
 					else:
 						move_state = 0
@@ -212,8 +220,7 @@ def PlayGame():
 						continue
 
 					redraw_board(window, board)
-
-					window['_historik_'].Update(pgn)
+					window['_historik_'].Update(pgn[-10:])
 					finalized = True
 
 					break
--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 Det unika med BastardChess är att man får reda på några av de bästa dragen innan man drar.  
 Detta är alltså en slags hybrid av hybridschack.  
 Meningen är att nya spelare snabbare ska få en känsla för vilka drag som är lämpliga.  
-Genom att det bästa draget utelämnats, uppmuntras man att leta efter det.
+Genom att det bästa draget utelämnas, uppmuntras man att leta efter det.  
+Hittar man inte det bästa draget, kan man försöka hitta det näst bästa draget.  
 
 # Sortering
 
@@ -20,18 +21,21 @@ Här anges vilken pjäs bonden ska förvandlas till.
 
 # Ledtrådar
 
+* 0: Inga ledtrådar
 * 123
 * 1234
 * 12345
-* 234: Man får se de tre starkaste dragen förutom det bästa.
+* 234: Man ser de tre starkaste dragen förutom det bästa.
 * 2345
 * 23456
 
-Tänk på att Stockfish inte är perfekt. Stockfish väljer inte samma drag varje gång. Dessutom påverkas dragens kvalitet av hur lång tid som används. 
+Tänk på att Stockfish inte är perfekt.  
+Stockfish väljer inte samma drag varje gång.  
+Dessutom påverkas dragens kvalitet av hur lång tid som används.  
 
 # Analys
 
-Dragen kopieras till klippbordet och man skickas till Lichess.
-Där klistrar man in partiet och kan sedan analysera.
+Dragen kopieras till klippbordet och man skickas till Lichess.  
+Där klistrar man in partiet och kan sedan analysera.  
 
 # Nytt parti
